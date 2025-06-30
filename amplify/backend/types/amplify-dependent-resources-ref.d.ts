29d445ef2b80b571df81113e6739a15042e05fd0
Aditya Prathi <adityaprathi2005@gmail.com>
2025-06-30 22:20:07 +0530

    Commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "securefileshare": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "securefileshare0c561650": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "fileupload": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
